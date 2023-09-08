af108ae6cad8895bc6a7fbcbd06fe7b45543665d
Ahmet Kartal <142317205+ahmetkartalofficial@users.noreply.github.com>
2023-09-08 18:53:32 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -64,13 +64,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Umut Yildiz</title>
+        <title>Ahmet Kartal</title>
         <link rel="icon" href="/favicon.ico" />
         <script src="/js/NotAtAllInteresting.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* Primary Meta Tags */}
-        <title>Umut Yildiz's Portfolio</title>
+        <title>Ahmet Kartal Portfolio</title>
         <meta name="title" content="Ahmet Kartal Portfolio" />
         <meta
           name="description"
